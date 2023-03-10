31629115f6c517694484dfa4d56d51a8b8a26fcb
Luiz Vitor Martinez Cardoso <luiz.martinez@geeksys.com.br>
2023-03-10 13:47:19 -0300

    shrink

--- src/useTextAreaAutoSizing.ts
+++ src/useTextAreaAutoSizing.ts
@@ -8,11 +8,8 @@ const useTextAreaAutoSizing = (
     if (textAreaRef) {
       let s = 0;
 
-      if (textAreaRef.innerHTML.length == 0)
-        s = -1;
-
-      if (textAreaRef.innerHTML.length > 1)
-        s = +1;
+      if (textAreaRef.innerHTML.length == 0) s = -1;
+      if (textAreaRef.innerHTML.length > 1) s = +1;
 
       textAreaRef.style.height = "0px";
       textAreaRef.style.height = s + textAreaRef.scrollHeight + "px";
